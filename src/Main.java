2dbab55bf75653ca8f671f128d9adb353d27a5cf
nooonseeense <egor-bolt12@yandex.ru>
2022-05-27 10:51:06 +0300

    First version

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,47 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        // Поехали!
+        Scanner scanner = new Scanner(System.in);
+        MonthlyReport monthlyReport = new MonthlyReport();
+        YearlyReport yearlyReport = new YearlyReport();
+
+        System.out.println("Добро пожаловать в «Автоматизация бухгалтерии [Build 1.0]»!");
+        while (true) {
+            printMenu();
+            switch (scanner.nextInt()) {
+                case 1:
+                    // method
+                    break;
+                case 2:
+                    // method
+                    break;
+                case 3:
+                    // method
+                    break;
+                case 4:
+                    // method
+                    break;
+                case 5:
+                    // method
+                    break;
+                case 1703:
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("SYSTEM: <ТАКОЙ КОМАНДЫ НЕ СУЩЕСТВУЕТ>\n");
+            }
+        }
+    }
+    private static void printMenu() {
+        System.out.println(
+                "Выберите команду из списка и нажмите Enter:\n" +
+                "[1] Считать все месячные отчёты\n" +
+                "[2] Считать годовой отчёт\n" +
+                "[3] Сверить отчёты\n" +
+                "[4] Вывести информацию о всех месячных отчётах\n" +
+                "[5] Вывести информацию о годовом отчёте"
+        );
     }
 }
 
---
+++ src/MonthlyReport.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MonthlyReport {
+
+    private String readFileContentsOrNull(String path) {
+        //String file = "src\\reports\\m.202101.csv";
+
+    }
+}
+
+//    BufferedReader reader = null;
+//        String line = "";
+//
+//        try {
+//            reader = new BufferedReader(new FileReader(path));
+//            while ((line = reader.readLine()) != null) {
+//
+//                String[] row = line.split(",");
+//
+//                for (String index : row) {
+//                    System.out.printf("%-25s", index);
+//                }
+//                System.out.println();
+//            }
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        finally {
+//            try {
+//                reader.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+
